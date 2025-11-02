94b7396a45a8d72acda85221f5dbd51490b05f57
Angelo González <angelo.andresgl@gmail.com>
2025-11-02 18:16:06 -0300

    Refactor main.py to improve code structure

--- main.py
+++ main.py
@@ -9,16 +9,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 #Modelos
-from sklearn.linear_model import LinearRegression      #Modelo 1 (Regresión)
-from sklearn.linear_model import LogisticRegression    #Modelo 2 (Clasificación)
+from sklearn.linear_model import LinearRegression    #Modelo 1 (Regresión)
+from sklearn.linear_model import LogisticRegression  #Modelo 2 (Clasificación)
 from sklearn.ensemble import RandomForestClassifier  #Modelo 3 (Árbol/RF)
-# Se eliminó la Red Neuronal de los imports
 
 #Métricas de Evaluación
 from sklearn.metrics import r2_score, mean_squared_error   # Para Regresión
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report # Para Clasificación
-
-# --- 1. CARGA Y LIMPIEZA DE DATOS ---
 
 #Carga y preprocesamiento de datos
 file_name = 'Cantidad de turistas.csv' 
@@ -253,5 +250,6 @@ plt.ylabel('Cantidad de Turistas Extranjeros')
 plt.legend()
 plt.grid(True)
 plt.show()
+
 
 print("\n--- SCRIPT ÚNICO COMPLETADO ---")
